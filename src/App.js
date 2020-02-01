a0b045da30bf071e59c088f7edf2bc7c2c166060
sarahxlee <sarahxlee@users.noreply.github.com>
2020-02-01 17:32:20 -0500

    dff

--- src/App.js
+++ src/App.js
@@ -21,8 +21,31 @@ import HomePage from './HomePage'
 
 
   class App extends Component {
+    
+    state = {
+      fields: {}
+    };
+
+    onChange = updatedValue => {
+      this.setState({
+        fields: {
+          ...this.state.fields,
+          ...updatedValue
+        }
+      });
+    };
+    
     render() {
       return (
+        <div className="App">
+         <Form onChange={fields => this.onChange(fields)} />
+         <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+         </p>
+      </div>
+      )
+      
+
 
       <BrowserRouter>
         <div>
@@ -35,9 +58,9 @@ import HomePage from './HomePage'
       </BrowserRouter>
 
 
-      )
+      
     }
-  }
+  
   // class App extends Component {
   //   render() {
   //     return (
--- src/CharityForm.js
+++ src/CharityForm.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react' ;
 
-class charityform extends Component {
+export default class charityform extends React.Component {
+	state = {
+		charityName: "",
+		mission: "",
+		needs: ""
+	};
+
+	change = e => {
+		this.props.onChange({ [e.target.name]: e.target.value });
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.setState({
+			charityName: "",
+			mission: "",
+			needs: ""
+	});
+	this.props.onChange({
+		charityName: "",
+			mission: "",
+			needs: ""
+	});
+};
 	render() {
-		return (<h1>This is the charity form</h1>);
+		return (
+			<form>
+				<input
+				name="charityName"
+				placeholder=""
+				value={this.state.charityName}
+				onChange={e=>this.change(e)}
+				/>
+				<br />
+				<input
+				mission="mission"
+				placeholder=""
+				value={this.state.mission}
+				onChange={e=>this.change(e)}
+				/>
+				<br />
+				<input
+				 needs="needs"
+				placeholder=""
+				value={this.state.needs}
+				onChange={e=>this.change(e)}
+				/>
+				<br />
+				<br />
+				 <button onClick={e => this.onSubmit(e)}>Submit</button>
+			</form>
+		)
 	}
 }
 
-export default charityform;
+//class charityform extends Component {
+//	render() {
+//		return (
+//		<div className="">);
+//	}
+//}
+
+//export default charityform;
