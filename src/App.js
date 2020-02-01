19ba5fde0b869f886d7cfbc827eacdbd73200526
sarahxlee <sarahxlee@users.noreply.github.com>
2020-02-01 11:11:02 -0500

    button added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
 
-export default App;
+
+    <p>
+    Welcome to Summation
+    <p>
+    I am a...
+    </p>
+    </p>
+    <buttonGroup variant="text" color="primary" aria-label="text primary button group">
+    <button>Business</button>
+    <button>Charity</button>
+    </buttonGroup>
+    </header>
+    </div>
+    );
+  }
+
+  export default App;
