f2a8d043c59f56afdc3a99ea6ccb69d562868a30
sarahxlee <sarahxlee@users.noreply.github.com>
2020-02-01 13:45:27 -0500

    ghfg

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import businessform from './businessform.js'
 
-class App extends Component {
+class Example extends Component {
   render() {
-  return (
-    <div className="App">
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-
-
-    <p>
-    Welcome to Summation
-    <p>
-    I am a...
-    </p>
-    </p>
-    <buttonGroup variant="text" color="primary" aria-label="text primary button group">
-    <button>Business</button>
-    <button>Charity</button>
-    </buttonGroup>
-    </header>
-    </div>
-    );
+    return (
+      <div>
+      <h1>Example page </h1>
+      <Link to="/businessform"> Go back home </Link>
+      </div>
+      )
+    }
   }
-}
 
-  export default App;
+  class App extends Component {
+    render() {
+      return (
+      <div>
+      <BrowserRouter>
+      <Switch>
+      <Route path="/businessform" component={businessform} eact />
+      <Route path="/example" component={Example} exact />
+      </Switch>
+      </BrowserRouter>
+      
+      <h1>Welcome to Summation</h1>
+      <h2>I am a... </h2>
+      
+      <button onclick="http://localhost:3000/businessform">Business</button>
+      
+      <button>Charity</button>
+      </div>
+
+      )
+    }
+  }
+
+  // class App extends Component {
+//   render() {
+//   return (
+//     <BrowserRouter>
+//     <Switch>
+//     <Route path= "/businessform" component= {businessform} exact />
+//     </Switch>
+//     </BrowserRouter>
+
+//     <div className="App">
+//     <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+
+
+//     <p>
+//     Welcome to Summation
+//     <p>
+//     I am a...
+//     </p>
+//     </p>
+//     <buttonGroup variant="text" color="primary" aria-label="text primary button group">
+//     <button>Business</button>
+//     <button>Charity</button>
+//     </buttonGroup>
+//     </header>
+//     </div>
+//     );
+//   }
+// }
+
+export default App
---
+++ src/businessform.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react' ;
+
+class businessform extends Component {
+	render() {
+		return (<h1>This is the business form</h1>);
+	}
+}
+
+export default businessform;
